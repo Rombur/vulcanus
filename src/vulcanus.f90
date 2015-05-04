d927bd20eb0ea181cfdb311e5b0d32191f3ed0bf
Bruno Turcksin <bruno.turcksin@gmail.com>
2015-05-03 21:41:11 -0500

    Initial commit.

---
+++ src/vulcanus.f90
@@ -0,0 +1,10 @@
+! Copyright (c) 2015, Bruno Turcksin.
+!
+! This file is subject to the Modified BSD License and may not be distributed
+! without copyright and license information. Please refer to the file
+! license.txt for the text and further information on this license.
+
+program vulcanus
+  print *, 'Start Vulcanus'
+  print *, 'Read the input file'
+end program vulcanus  
